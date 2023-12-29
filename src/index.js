bb83bc34f6df146877df83387feb93c67d106073
leticiavenansi <leticiavenansi@hotmail.com>
2023-12-29 15:42:24 +0100

    Added falling hearts

--- src/index.js
+++ src/index.js
@@ -92,3 +92,21 @@ myForm.addEventListener("submit", showMyCity)
 
 let baesForm = document.querySelector("#baesCity")
 baesForm.addEventListener("submit", showBaesCity)
+
+function createHeart() {
+  const heart = document.createElement('div');
+  heart.classList.add('heart');
+  
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+  
+  heart.innerText = '💗';
+  
+  document.body.appendChild(heart);
+  
+  setTimeout(() => {
+              heart.remove();
+              }, 5000);
+}
+
+setInterval(createHeart, 300);
