3f0c29a5c3bd27135d4ed4859796d395726a6dfc
leticiavenansi <leticiavenansi@hotmail.com>
2024-01-06 19:04:44 +0100

    Cleaned code

--- src/index.js
+++ src/index.js
@@ -14,30 +14,10 @@ if (minutes < 10) {
 }let year = now.getFullYear();
 
 
-let days = [
-  "Sunday", 
-  "Monday", 
-  "Tuesday", 
-  "Wednesday", 
-  "Thursday", 
-  "Friday", 
-  "Saturday"];
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let day = days[now.getDay()];
 
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-  ];
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let month = months[now.getMonth()];
 
 h2.innerHTML = `${month} ${date}, ${year} - ${day} - ${hours}h${minutes}`;
